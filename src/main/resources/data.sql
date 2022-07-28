d0368a2e0ed229d2801298e0631776998a8ee729
VVasilev <vvasilev@sitronics.com>
2022-07-28 18:41:09 +0300

    updated schema and data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO holiday (date) VALUES
+INSERT INTO other_services.holiday (date) VALUES
 ('2022-01-01 00:00:00'),
 ('2022-01-02 00:00:00'),
 ('2022-01-03 00:00:00'),
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS holiday;
+DROP TABLE IF EXISTS other_services.holiday;
 
-CREATE TABLE holiday (
+CREATE TABLE other_services.holiday (
     id serial primary key,
 --     id uuid primary key default gen_random_uuid(),
     date timestamp without time zone not null
